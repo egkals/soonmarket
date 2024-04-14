a6a50d42e623b1c88924ff47708da5791a8a3e72
hamin <https://github.com/egkals/soonmarket>
2024-04-14 17:23:40 +0900

    Signup.js 코드 수정 및 닉네임 추가

--- src/components/UserPage/SignupPage/Signup.js
+++ src/components/UserPage/SignupPage/Signup.js
@@ -3,28 +3,27 @@ import './Signup.css';
 import axios from 'axios';
 
 function SingupPage() {
-    // 비밀번호와 비밀번호 확인을 위한 상태 정의
-    
-    
+
+    // 이름, 아이디, 패스워드, 패스워드 체크, 이메일, 닉네임, 전화번호, 주소 상태 정의
+    const [mb_name, setmb_name] = useState('');
+    const [mb_id, setmb_id] = useState('');
+    const [mb_passwd, setmb_passwd] = useState('');
+    const [mb_passwd_ck, setmb_passwd_ck] = useState('');
+    const [mb_email, setmb_email] = useState('');
+    const [mb_nickname, setmb_nickname] = useState('');
+    const [mb_tel, setmb_tel] =useState('');
+    const [mb_addr, setmb_addr] = useState('');
+
     // 비밀번호 일치 여부를 나타내는 상태 정의
     const [passwordError, setPasswordError] = useState('');
-    // 전화번호 확인을 위한 상태 정의
-
-    // 전화번호 형식 여부 나타내는 상태 정의
+    // 전화번호 오류 여부 나타내는 상태 정의
     const [telError, setTelError] = useState('');
+
     // 아이디와 아이디 사용 가능 상태를 위한 상태 정의
     // const [id,setId] = useState('');
     // const [availableId, setAvailableId] = useState('');
     // 아이디 중복 여부 상태 정의
     // const [idError, setIdError] = useState('');
-    
-    const [mb_id, setmb_id] = useState('');
-    const [mb_passwd, setmb_passwd] = useState('');
-    const [mb_passwd_ck, setmb_passwd_ck] = useState('');
-    const [mb_email, setmb_email] = useState('');
-    const [mb_name, setmb_name] = useState('');
-    const [mb_tel, setmb_tel] =useState('');
-    const [mb_addr, setmb_addr] = useState('');
 
     // //아이디 중복 체크를 어디로?
     // // 아이디를 적었을때 함수
@@ -54,54 +53,54 @@ function SingupPage() {
 
 
     // Submit 누를시 호출되는 함수
-const handleSubmit = async (e) => {
-    e.preventDefault(); // 기본 제출 이벤트 막기
+    const handleSubmit = async (e) => {
+        e.preventDefault(); // 기본 제출 이벤트 막기
 
-    const formData = new FormData();
-    formData.append('mb_id', mb_id);
-    formData.append('mb_passwd', mb_passwd);
-    formData.append('mb_email', mb_email);
-    formData.append('mb_name', mb_name);
-    formData.append('mb_tel', mb_tel); // 오타 수정
-    formData.append('mb_addr', mb_addr);
+        const formData = new FormData();
+        formData.append('mb_name', mb_name);
+        formData.append('mb_id', mb_id);
+        formData.append('mb_passwd', mb_passwd);
+        formData.append('mb_email', mb_email);
+        formData.append('mb_nickname', mb_nickname);
+        formData.append('mb_tel', mb_tel); // 오타 수정
+        formData.append('mb_addr', mb_addr);
 
-    // 비밀번호와 비밀번호 확인이 일치하지 않으면 에러 메시지 출력
-    if (mb_passwd !== mb_passwd_ck) {
-        setPasswordError('비밀번호가 일치하지 않습니다.');
-        return; // 함수 종료
-    } else {
-        setPasswordError('');
-    }
-
-    if (!valid_ph(mb_tel)){
-        setTelError('올바른 형식이 아닙니다. 다시 확인해주세요.');
-        return; // 함수 종료
-    } else {
-        setTelError('');
-    }
-
-    try {
-        const response = await axios.post('http://localhost:8080/api/members', formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data',
-            },
-        });
-
-        console.log(response.data);
-        // 성공 시 추가 작업 수행
-    } catch (error) {
-        if (error.response) {
-            // 서버에서 반환한 에러 메시지 출력
-            console.error(error.response.data);
-        } else if (error.request) {
-            // 요청이 발생했으나 응답을 받지 못한 경우
-            console.error('네트워크 에러가 발생했습니다.');
+        // 비밀번호와 비밀번호 확인이 일치하지 않으면 에러 메시지 출력
+        if (mb_passwd !== mb_passwd_ck) {
+            setPasswordError('비밀번호가 일치하지 않습니다.');
+            return; // 함수 종료
         } else {
-            // 그 외의 에러
-            console.error('에러가 발생했습니다.', error.message);
+            setPasswordError('');
         }
-    }
-};
+
+        if (!valid_ph(mb_tel)){
+            setTelError('올바른 형식이 아닙니다. 다시 확인해주세요.');
+            return; // 함수 종료
+        } else {
+            setTelError('');
+        }
+
+        try {
+            const response = await axios.post('http://localhost:8080/api/members', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            console.log(response.data);
+            // 성공 시 추가 작업 수행
+        } catch (error) {
+            if (error.response) {
+                // 서버에서 반환한 에러 메시지 출력
+                console.error(error.response.data);
+            } else if (error.request) {
+                // 요청이 발생했으나 응답을 받지 못한 경우
+                console.error('네트워크 에러가 발생했습니다.');
+            } else {
+                // 그 외의 에러
+                console.error('에러가 발생했습니다.', error.message);
+            }
+        }
+    };
 
 
     return (
@@ -112,6 +111,19 @@ const handleSubmit = async (e) => {
             <body>
                 <div className="form-elements">
                     <form id="form-elements" onSubmit={handleSubmit}>
+                        {/* 이름 */}
+                        <div className='form-element'>
+                            <input 
+                                type='text' 
+                                id='mb_name' 
+                                name='mb_name'
+                                placeholder='Name' 
+                                maxLength="255" 
+                                className="form-control"
+                                value={mb_name}
+                                onChange={(e) => setmb_name(e.target.value)}
+                            />
+                        </div>
                         {/* 아이디 */}
                         <div className='form-element'>
                             <input 
@@ -143,6 +155,9 @@ const handleSubmit = async (e) => {
                                 onChange={(e) => setmb_passwd(e.target.value)}
                             />
                         </div>
+                        {/* 비밀번호 일치 여부에 따른 에러 메시지 */}
+                        {passwordError && <div style={{ color: 'red' }}>{passwordError}</div>}
+                        {/* 이메일 */}
                         {/* 비밀번호 체크 */}
                         <div className='form-element'>
                             <input 
@@ -156,9 +171,6 @@ const handleSubmit = async (e) => {
                                 onChange={(e) => setmb_passwd_ck(e.target.value)}
                             />
                         </div>
-                        {/* 비밀번호 일치 여부에 따른 에러 메시지 */}
-                        {passwordError && <div style={{ color: 'red' }}>{passwordError}</div>}
-                        {/* 이메일 */}
                         <div className='form-element'>
                             <input 
                                 type='text' 
@@ -175,15 +187,17 @@ const handleSubmit = async (e) => {
                         <div className='form-element'>
                             <input 
                                 type='text' 
-                                id='mb_name' 
-                                name='mb_name'
-                                placeholder='Name' 
+                                id='mb_nickname' 
+                                name='mb_nickname'
+                                placeholder='NickName' 
                                 maxLength="255" 
                                 className="form-control"
-                                value={mb_name}
-                                onChange={(e) => setmb_name(e.target.value)}
+                                value={mb_nickname}
+                                onChange={(e) => setmb_nickname(e.target.value)}
                             />
                         </div>
+                        {/* 전화번호 형식이 아니면 에러 메시지 */}
+                        {telError && <div style={{ color: 'red' }}>{telError}</div>}
                         {/* 전화번호 */}
                         <div className='form-element'>
                             <input 
@@ -196,8 +210,6 @@ const handleSubmit = async (e) => {
                                 onChange={(e) => setmb_tel(e.target.value)}
                             />
                         </div>
-                        {/* 전화번호 형식이 아니면 에러 메시지 */}
-                        {telError && <div style={{ color: 'red' }}>{telError}</div>}
                         {/* 주소 */}
                         <div className='form-element'>
                             <input 
